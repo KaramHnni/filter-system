b95dfa24b61b532e1ddc9e449267b85f6db0bf1d
Henni Karam <karam.tenes@gmail.com>
2018-09-07 11:46:08 +0100

    setup layout site head

---
+++ resources/views/layouts/site.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>@yield('page-title')</title>
+
+        <!-- Fonts -->
+        <link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet">
+        <link href="{{ asset('/assets/css/font-jannat.css') }}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('/assets/css/font-din-next.css') }}" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <link href="{{ asset('/assets/css/tailwind.min.css') }}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('/assets/css/site.css') }}" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+    </body>
+</html>
