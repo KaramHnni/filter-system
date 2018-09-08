3aee1187962607f1250d950ea4c4f3edf113eaaa
Henni Karam <karam.tenes@gmail.com>
2018-09-08 15:55:43 +0100

    add city and area methods

--- app/City.php
+++ app/City.php
@@ -7,4 +7,31 @@ use Illuminate\Database\Eloquent\Model;
 class City extends Model
 {
 
+    public function getFullNameAttribute(){
+        return "مدينة $this->name" ;
+    }
+    public function getStatusAttribute(){
+        if($this->status == 0){
+            return "غير مفعل";
+        }
+        if($this->status == 1){
+            return "مفعل";
+        }
+    }
+
+    public function active(){
+        return self::where('status',1);
+    }
+
+    public function setActive(){
+        $this->status = 1;
+        $this->updated_at = now();
+        $this->save();
+    }
+
+    public function setInactive(){
+        $this->status = 0;
+        $this->updated_at = now();
+        $this->save();
+    }
 }
