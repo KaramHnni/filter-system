803782b4507a5fc5c5790e9547d085bfb7ea4f3a
Henni Karam <karam.tenes@gmail.com>
2018-09-08 14:50:52 +0100

    add redirectToDashboard method

--- app/Http/Controllers/User/IndexController.php
+++ app/Http/Controllers/User/IndexController.php
@@ -7,5 +7,7 @@ use App\Http\Controllers\Controller;
 
 class IndexController extends Controller
 {
-    //
+    public function redirectToDashboard(){
+        return redirect(route('user.dashboard'));
+    }
 }
